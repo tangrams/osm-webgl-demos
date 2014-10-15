b3367b81492d2e1d5811f380e9b16978933aa584
Peter Richardson <github@zoomy.net>
2014-10-15 17:13:45 -0400

    cleaned up layers

--- demo2/layers.js
+++ demo2/layers.js
@@ -4,41 +4,31 @@
         data: 'earth'
     },
     {
-        name: 'water',
-        data: 'water'
-    },
-    {
         name: 'buildings',
         data: 'buildings'
     },
     {
+        name: 'water',
+        data: 'water'
+    },
+    {
         name: 'landuse',
-        data: function (json) {
-            if (!json['landuse'] || !json['landuse'].features) {
-                return null;
-            }
+        data: 'landuse'
+        // data: function (json) {
+        //     if (!json['landuse'] || !json['landuse'].features) {
+        //         return null;
+        //     }
 
-            return {
-                type: 'FeatureCollection',
-                features: json['landuse'].features.sort(function(a, b) {
-                    return (b.properties.area - a.properties.area); 
-                })
-            };
-        }
+        //     return {
+        //         type: 'FeatureCollection',
+        //         features: json['landuse'].features.sort(function(a, b) {
+        //             return (b.properties.area - a.properties.area);
+        //         })
+        //     };
+        // }
     },
     {
         name: 'roads',
-        data: function (json) {
-            if (!json['roads'] || !json['roads'].features) {
-                return null;
-            }
-
-            return {
-                type: 'FeatureCollection',
-                features: json['roads'].features.sort(function(a, b) {
-                  return (a.properties.sort_key - b.properties.sort_key);
-                })
-            };
-        }
+        data: 'roads'
     },
 ]
