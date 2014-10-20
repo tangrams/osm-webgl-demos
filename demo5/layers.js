e3aad47e48f5156f456bea8dd0bcc6f6dc7730d4
Peter Richardson <github@zoomy.net>
2014-10-20 18:39:49 -0400

    add demo5

---
+++ demo5/layers.js
@@ -0,0 +1,25 @@
+[
+    {
+        name: 'earth',
+    },
+    {
+        name: 'water',
+    },
+    {
+        name: 'landuse',
+    },
+    {
+        name: 'roads',
+    },
+    {
+        name: 'cycleway',
+        data: 'roads'
+    },
+    {
+        name: 'bridges',
+        data: 'roads'
+    },
+    {
+        name: 'buildings',
+    },
+]
